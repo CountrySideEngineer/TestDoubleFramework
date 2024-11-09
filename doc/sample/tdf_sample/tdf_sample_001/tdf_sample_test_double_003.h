2e26a0293d6ff8abe290d8b9df08e7de51ea2ca4
CountrySideEngineer <deep-sleeper@nifty.com>
2024-11-10 08:18:39 +0900

    Add uncommitted header file.

---
+++ doc/sample/tdf_sample/tdf_sample_001/tdf_sample_test_double_003.h
@@ -0,0 +1,7 @@
+#pragma once
+
+BEGIN_DECLARE_TD(SampleSubFunc_003_001)
+DEC_PTR_ARG_P1_OUT(SampleSubFunc_003_001, BYTE, input1)
+DEC_VALUE_ARG(SampleSubFunc_003_001, ULONG, input2)
+DEC_VOID_FUNC(SampleSubFunc_003_001)
+END_DECLARE_TD(SampleSubFunc_003_001)
