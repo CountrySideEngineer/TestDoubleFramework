8380f65d968de49c573e5f3bb3e4c64c98d07ba6
CountrySideEngineer <deep-sleeper@nifty.com>
2024-11-09 23:11:07 +0900

    Add test case. Add a test case about a pointer argument.

--- doc/sample/tdf_sample/tdf_sample_001/test_driver.cpp
+++ doc/sample/tdf_sample/tdf_sample_001/test_driver.cpp
@@ -55,3 +55,23 @@ TEST_F(SampleFunc_002_test, 001)
 	ASSERT_EQ(1, PTR_ARG_VAL(SampleSubFunc_002_001, input1, 0, 0));
 	ASSERT_EQ(1, ARG_VAL(SampleSubFunc_002_001, input2, 0));
 }
+
+TEST_F(SampleFunc_002_test, 002)
+{
+	BYTE	_input1[10] = { 0 };
+	BYTE*	input1 = &_input1[0];
+	SHORT	input2 = 10;
+
+	for (int index = 0; index < input2; index++) {
+		_input1[index] = (index + 10);
+	}
+	SET_PTR_ARG_VAL_SIZE(SampleSubFunc_002_001, input1, 0, input2);
+
+	SampleFunc_002(input1, input2);
+
+	ASSERT_EQ(1, CALLED_COUNT(SampleSubFunc_002_001));
+	ASSERT_EQ(10, PTR_ARG_VAL(SampleSubFunc_002_001, input1, 0, 0));
+	ASSERT_EQ(11, PTR_ARG_VAL(SampleSubFunc_002_001, input1, 0, 1));
+	ASSERT_EQ(19, PTR_ARG_VAL(SampleSubFunc_002_001, input1, 0, 9));
+	ASSERT_EQ(10, ARG_VAL(SampleSubFunc_002_001, input2, 0));
+}
