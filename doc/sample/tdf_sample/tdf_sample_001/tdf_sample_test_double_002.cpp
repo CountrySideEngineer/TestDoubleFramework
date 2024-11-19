a7ba4bf69f08d27121b28a7253296bf3c543673d
CountrySideEngineer <deep-sleeper@nifty.com>
2024-11-19 19:39:23 +0900

    Update test sample codes.

--- doc/sample/tdf_sample/tdf_sample_001/tdf_sample_test_double_002.cpp
+++ doc/sample/tdf_sample/tdf_sample_001/tdf_sample_test_double_002.cpp
@@ -4,6 +4,6 @@
 
 BEGIN_DEF_TD(SampleSubFunc_002_001)
 REG_PTR_ARG_P1_IN(SampleSubFunc_002_001, BYTE, input1)
-REG_VALUE_ARG(SampleSubFunc_002_001, ULONG, input2)
-DEF_VOID_FUNC_2(SampleSubFunc_002_001, BYTE*, input1, ULONG, input2)
+REG_VAL_ARG(SampleSubFunc_002_001, ULONG, input2)
+DEF_VOID_FUNC(SampleSubFunc_002_001, BYTE*, input1, ULONG, input2)
 END_DEF_TD(SampleSubFunc_002_001)
--- doc/sample/tdf_sample/tdf_sample_001/tdf_sample_test_double_003.cpp
+++ doc/sample/tdf_sample/tdf_sample_001/tdf_sample_test_double_003.cpp
@@ -4,6 +4,6 @@
 
 BEGIN_DEF_TD(SampleSubFunc_003_001)
 REG_PTR_ARG_P1_OUT(SampleSubFunc_003_001, BYTE, input1)
-REG_VALUE_ARG(SampleSubFunc_003_001, ULONG, input2)
-DEF_VOID_FUNC_2(SampleSubFunc_003_001, BYTE*, input1, ULONG, input2)
+REG_VAL_ARG(SampleSubFunc_003_001, ULONG, input2)
+DEF_VOID_FUNC(SampleSubFunc_003_001, BYTE*, input1, ULONG, input2)
 END_DEF_TD(SampleSubFunc_003_001)
