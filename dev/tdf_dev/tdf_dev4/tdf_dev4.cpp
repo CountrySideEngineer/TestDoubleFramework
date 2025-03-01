0d18cf1aef9978ad6f578e76caa3bf125a917b00
CountrySideEngineer <deep-sleeper@nifty.com>
2025-03-01 22:15:26 +0900

    Update codes to develop the framework.

--- dev/tdf_dev/tdf_dev4/tdf_dev4.cpp
+++ dev/tdf_dev/tdf_dev4/tdf_dev4.cpp
@@ -6,32 +6,38 @@
 /*
 	void FuncA(void);
  */
-BEGIN_DEFINE_TD(funcA)
-DEFINE_ARG_BUFF(funcA, int, input1)
-DEFINE_PTR_ARG_VAL_BUFF(funcA, int, input2)
-DEFINE_PTR_ARG_VAL_SIZE_BUFF(funcA, int, input2)
-DEFINE_PTR_ARG_RETURN_VAL_BUFF(funcA, int, input2)
-DEFINE_PTR_ARG_RETURN_VAL_SIZE_BUFF(funcA, int, input2)
-END_DEFINE_TD(funcA)
+BEGIN_DEF_TD(FuncA);
+DEF_VOID_FUNC(FuncA);
+END_DEF_TD(FuncA);
 
-BEGIN_DEFINE_TD(funcB)
-REG_PTR_ARG_P1_IN(funcB, char, input1)
-DEFINE_VOID_FUNC_1(funcB, char*, input1)
-END_DEFINE_TD(funcB)
+/*
+	void FuncB(int input1)
+ */
+BEGIN_DEF_TD(FuncB);
+REG_VAL_ARG(FuncB, int, input1);
+DEF_VOID_FUNC(FuncB, int, input1)
+END_DEF_TD(FuncB)
 
-BEGIN_DEFINE_TD(funcC)
-REG_VALUE_ARG(funcC, int, input1)
-REG_PTR_ARG_P1_IN(funcC, char, input2)
-REG_PTR_ARG_P1_OUT(funcC, short, input3)
-DEFINE_VOID_FUNC_3(funcC, int, input1, char*, input2, short*, input3)
-END_DEFINE_TD(funcC)
+/*
+	void FuncB(int input1)
+ */
+BEGIN_DEF_TD(FuncC);
+REG_PTR_ARG_P1_IN(FuncC, int, input1);
+DEF_VOID_FUNC(FuncC, int*, input1);
+END_DEF_TD(FuncC)
 
-BEGIN_DEFINE_TD(funcD)
-DEFINE_VAL_FUNC_0(int, funcD)
-END_DEFINE_TD(funcD)
+#define	GET_NAME(str)	_T(# str)
 
 int main()
 {
+	int input1 = 0;
+
+	TD_INIT(FuncC);
+
+	FuncC(&input1);
+
+	_tprintf(_T("%s = %d\n"), GET_NAME(input1), input1);
+	_tprintf(_T("%s = %d\n"), GET_NAME(CALLED_COUNT(FuncC)), CALLED_COUNT(FuncC));
 	return 0;
 }
 
