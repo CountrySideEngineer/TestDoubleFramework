d4fb004477f82fd4922faaf7450aec7dcbb0704f
CountrySideEngineer <deep-sleeper@nifty.com>
2025-03-01 11:45:14 +0900

    Update sample codes of tdf.h. -Delete a comment out code. -Fix a bug in macro which defines a test double of funcC. -Add "main" method to enable the solution and project to build.

--- dev/tdf_dev/tdf_dev4/tdf_dev4.cpp
+++ dev/tdf_dev/tdf_dev4/tdf_dev4.cpp
@@ -15,7 +15,6 @@ DEFINE_PTR_ARG_RETURN_VAL_SIZE_BUFF(funcA, int, input2)
 END_DEFINE_TD(funcA)
 
 BEGIN_DEFINE_TD(funcB)
-//REG_PTR_ARG_P1_IN(funcB, char, input1)
 REG_PTR_ARG_P1_IN(funcB, char, input1)
 DEFINE_VOID_FUNC_1(funcB, char*, input1)
 END_DEFINE_TD(funcB)
@@ -24,13 +23,16 @@ BEGIN_DEFINE_TD(funcC)
 REG_VALUE_ARG(funcC, int, input1)
 REG_PTR_ARG_P1_IN(funcC, char, input2)
 REG_PTR_ARG_P1_OUT(funcC, short, input3)
-DEFINE_VOID_FUNC_3(funcC, int, input1, char*, input2, char*, input3)
+DEFINE_VOID_FUNC_3(funcC, int, input1, char*, input2, short*, input3)
 END_DEFINE_TD(funcC)
 
 BEGIN_DEFINE_TD(funcD)
 DEFINE_VAL_FUNC_0(int, funcD)
 END_DEFINE_TD(funcD)
 
-
+int main()
+{
+	return 0;
+}
 
 
